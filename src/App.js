9a8014681c7df5d404d6bc24c94ef8fc48b8b1a1
nikunjmavani <nikunjmavani@gmail.com>
2020-02-23 15:14:06 +0530

    cord format done

--- src/App.js
+++ src/App.js
@@ -58,22 +58,21 @@ function App() {
         data={array}
         title="Detail Panel With RowClick Preview"
         detailPanel={rowData => {
-          console.log("rowData",rowData)
           return (
             <MaterialTable
-      title="Render Image Preview"
-      columns={[
-        { title: 'Avatar', field: 'imageUrl', render: rowData => <img src={rowData.imageUrl} style={{width: 40, borderRadius: '50%'}} alt="userProfilePic"/> },
-        { title: 'Name', field: 'name' },
-        { title: 'Surname', field: 'surname' },
-        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-        {title: 'Birth Place',field: 'birthCity',lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },},
-      ]}
-      data={[
-        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63, imageUrl: 'https://avatars0.githubusercontent.com/u/7895451?s=460&v=4' },
-        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34, imageUrl: 'https://avatars0.githubusercontent.com/u/7895451?s=460&v=4' },
-      ]}        
-    />
+              title="Render Image Preview"
+              columns={[
+                { title: 'Avatar', field: 'imageUrl', render: rowData => <img src={rowData.imageUrl} style={{width: 40, borderRadius: '50%'}} alt="userProfilePic"/> },
+                { title: 'Name', field: 'name' },
+                { title: 'Surname', field: 'surname' },
+                { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+                {title: 'Birth Place',field: 'birthCity',lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },},
+              ]}
+              data={[
+                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63, imageUrl: 'https://avatars0.githubusercontent.com/u/7895451?s=460&v=4' },
+                { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34, imageUrl: 'https://avatars0.githubusercontent.com/u/7895451?s=460&v=4' },
+              ]}        
+            />
           )
         }}
         onRowClick={(event, rowData, togglePanel) => togglePanel()}
