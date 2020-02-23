e4aa72d138819ba7101f7b51f2476608d6a5ef99
nikunjmavani <nikunjmavani@gmail.com>
2020-02-23 12:26:15 +0530

    add csv to array

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [imageFileURL, setImageFileURL] = useState();
+  const [aray, setArray] = useState([]);
+
+
+  function handleChangeCsv(e) {
+    // const file = e.target.files[0];
+    // console.log("csv", e.target.files, file);
+    var file = e.target.files[0];
+    var reader = new FileReader();
+     reader.readAsText(file);
+    // console.log("reader", reader);
+
+    reader.onload = () => {
+      setImageFileURL(reader.result);
+    };
+  }
+
+  function csvJSON(csv = imageFileURL) {
+    console.log("csv", imageFileURL);
+    var lines = imageFileURL.split(/\r?\n|\r/);
+    console.log("lines", lines);
+
+    var result = [];
+
+    var headers = lines[0].split(",");
+
+    for (var i = 1; i < lines.length - 1; i++) {
+      var obj = {};
+      var currentline = lines[i].split(",");
+
+      for (var j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j];
+      }
+
+      result.push(obj);
+    }
+
+    //return result; //JavaScript object
+    // return setArray(JSON.stringify(result)); //JSON
+    return setArray(result);
+  }
+
+  console.log("array", aray);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <label htmlFor="myfile">Select files:</label>
+      <input
+        type="file"
+        id="myfile"
+        name="myfile"
+        multiple
+        onChange={handleChangeCsv}
+      />
+      <input type="submit" onClick={csvJSON} />
     </div>
   );
 }
